c831c825e8e4b29ecb9c7bafb0fcd5111efcc8af
ametaireau <ametaireau@gmail.com>
2010-04-16 18:58:54 +0200

    make test pass

--- pypiclient/pypiclient/tests.py
+++ pypiclient/pypiclient/tests.py
@@ -19,19 +19,19 @@ class TestXmlRpcClient(unittest.TestCase):
         """
         self.client = client.XmlRpcClient()
 
-    def _mock_xmlrpc_method(method_name, return_value):
-        self.client._get_server_proxy = Mock()
-        proxy = Mock(xmlrpclib.ServerProxy())
-        method = getattr(proxy, method_name)
+    def _mock_xmlrpc_method(self, method_name, return_value):
+        self.client._server_proxy = Mock(xmlrpclib.ServerProxy)()
+        setattr(self.client._server_proxy, method_name, Mock())
+        method = getattr(self.client._server_proxy, method_name)
         method.return_value = return_value
         return method
     
     def test_init(self):
-        c = XmlRpcClient()
+        c = client.XmlRpcClient()
         self.assertEqual(c.server_url, client.PYPI_XML_RPC_URL, 
             "when no server_url is defined, should use client.PYPI_XML_RPC_URL")
 
-        c = XmlRpcClient('http://a/test/url')
+        c = client.XmlRpcClient('http://a/test/url')
         self.assertEqual(c.server_url, 'http://a/test/url')
 
     def test_get_server_proxy(self):
@@ -42,14 +42,12 @@ class TestXmlRpcClient(unittest.TestCase):
         self.assertFalse(hasattr(self.client, '_server_proxy'))
         proxy1 = self.client._get_server_proxy()
         self.assertTrue(hasattr(self.client, '_server_proxy'))
-        proxy2 = self.client._get_server_proxy()
-        self.assertEqual(proxy1, proxy2)
+        self.assertTrue(proxy1 is self.client._get_server_proxy())
 
     def test_get_project_versions(self):
         self._mock_xmlrpc_method('package_releases', ['1.1', '1.2', '1.3'])
         versions = self.client.get_project_versions('Foo')
-        self.assertEqual(versions, ['1.1', '1.2', '1.3'], 
-            'a list of value must remain a list of values')
+        self.assertEqual(versions, ['1.1', '1.2', '1.3']) 
 
         self._mock_xmlrpc_method('package_releases', [])
         self.assertRaises(client.ProjectDoesNotExist, 
@@ -69,13 +67,13 @@ class TestXmlRpcClient(unittest.TestCase):
                 'packagetype': 'bdist', 
             }
         ])
-        self.assertEqual(self.get_project_url('Foo', '1.3'), 
+        self.assertEqual(self.client.get_project_url('Foo', '1.3'), 
             'http://pypi.python.org/packages/source/g/foo/foo-1.3.tar.gz')
 
         self.client.get_project_versions = Mock()
         self.client.get_project_versions.return_value = ['1.1', '1.2', '1.3']
 
-        self.assertEqual(self.get_project_url('Foo'), 
+        self.assertEqual(self.client.get_project_url('Foo'), 
             'http://pypi.python.org/packages/source/g/foo/foo-1.3.tar.gz')
 
         self.assertTrue(self.client.get_project_versions.called)
